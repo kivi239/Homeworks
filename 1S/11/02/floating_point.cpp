d3c13a3005a532a82056f748f4fffab7660bfe10
kivi239 <kivi239@gmail.com>
2013-12-09 19:33:38 +0400

    task 11/02 is done!

---
+++ 1S/11/02/floating_point.cpp
@@ -0,0 +1,105 @@
+#include <cstdio>
+#include <cstring>
+
+enum States
+{
+  startCode,
+  numberCode,
+  numberSignCode,
+  startFracPartCode,
+  fracPartCode,
+  startExpCode,
+  expCode,
+  expSignCode,
+  errorCode,
+};
+
+const int maxLen = 1050;
+
+bool isDigit(char c)
+{
+  return ('0' <= c && c <= '9');
+}
+
+bool isCorrect(char *number, int length)
+{
+  int state = startCode;
+  for (int i = 0; i < length; i++)                                                                                                  
+  {
+    char cur = number[i];
+    if (state == startCode)
+    {
+      if (isDigit(cur))
+        state = numberCode;
+      else if (cur == '-')
+        state = numberSignCode;
+      else 
+        state = errorCode;
+    }
+    else if (state == numberSignCode)
+    {
+      if (isDigit(cur))
+        state = numberCode;
+      else
+        state = errorCode;
+    }
+    else if (state == numberCode)
+    {
+      if (cur == '.')
+        state = startFracPartCode;
+      else if (!isDigit(cur))
+        state = errorCode;                                                         
+    }
+    else if (state == startFracPartCode)
+    {
+      if (isDigit(cur))                                                                                         
+        state = fracPartCode;
+      else
+        state = errorCode;
+    }
+    else if (state == fracPartCode)
+    {
+      if (cur == 'E')
+        state = startExpCode;
+      else if (!isDigit(cur))
+        state = errorCode;
+    }
+    else if (state == startExpCode)
+    {
+      if (isDigit(cur))
+        state = expCode;
+      else if (cur == '+' || cur == '-')
+        state = expSignCode;
+      else 
+        state = errorCode;
+    }
+    else if (state == expSignCode)
+    {
+      if (isDigit(cur))
+        state = expCode;
+      else
+        state = errorCode;
+    }
+    else if (state == expCode)
+    {
+      if (!isDigit(cur))
+        state = errorCode;
+    }
+  }
+
+  return (state == numberCode || state == fracPartCode || state == expCode);
+}
+
+int main()
+{
+  printf("Please, enter a floating-point number\n");
+  char *number = new char[maxLen];
+  scanf("%s", number);
+
+  if (isCorrect(number, strlen(number)))
+    printf("It is a correct notation\n");
+  else 
+    printf("It is an incorrect notation\n");
+
+  return 0;
+}
