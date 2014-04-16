51e81cf5c4c8a5e035823b816717e2316bb11b97
kivi239 <kivi239@gmail.com>
2014-04-16 13:40:35 +0400

    Task 5.2 is done.

---
+++ 2S/05/2/main.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include "set.h"
+#include <iostream>
+
+int main()
+{
+  Set<int> s;
+  s.add(5);
+  s.add(2);
+  cout << s.find(2) << ' ' << s.find(6) << endl;
+  s.print();
+  s.add(3);
+  s.remove(5);
+
+  Set<int> s2;
+  s2.add(2);
+  s2.add(4);
+  s2.print();
+
+  Set<int> s3 = s2.intersec(s);
+  s3.print();
+
+  Set<int> s4;
+  s4.add(9);
+  Set<int> s5 = s4.un(s);
+  s5.print();
+
+  return 0;
+}    
---
+++ 2S/05/2/set.h
@@ -0,0 +1,141 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class Set
+{
+public:
+  Set();
+  ~Set();
+  void print();
+  void add(T);
+  bool find(T);
+  void remove(T);
+  Set intersec(Set &);
+  Set un(Set &);
+
+private:
+  struct SetElement
+  {
+    T value;
+    SetElement *next;
+    SetElement() {}
+    SetElement(T _value, SetElement *_next) : value(_value), next(_next) {}
+  };
+                                                     
+  SetElement *head;
+};
+
+template<typename T>
+Set<T>::Set() : head(new SetElement(T(), nullptr)) {}
+
+template<typename T>
+Set<T>::~Set()                              
+{
+  SetElement *curElement = head;
+  while (curElement != nullptr)
+  {
+    SetElement *tmp = curElement->next;
+    delete curElement;
+    curElement = tmp;
+  }
+}
+
+template<typename T>
+void Set<T>::add(T value)                                                      
+{
+  SetElement *curElement = head;
+  while (curElement->next != nullptr && curElement->next->value < value)
+    curElement = curElement->next;
+  
+  if (curElement->next != nullptr && curElement->next->value == value)  
+    return;
+
+  SetElement *newElement = new SetElement(value, curElement->next);
+  curElement->next = newElement;
+}
+
+template<typename T>
+void Set<T>::print()
+{
+  SetElement *curElement = head->next;
+  while (curElement != nullptr)
+  {
+    cout << curElement->value << ' ' ;
+    curElement = curElement->next;
+  }
+  cout << endl;
+}
+
+template<typename T>
+bool Set<T>::find(T value)
+{
+  SetElement *curElement = head;
+  while (curElement->next != nullptr && curElement->next->value < value)
+    curElement = curElement->next;
+  
+  return (curElement->next != nullptr && curElement->next->value == value);  
+} 
+
+template<typename T>
+void Set<T>::remove(T value)
+{
+  if (!this->find(value))
+    return;
+  
+  SetElement *curElement = head;
+  while (curElement->next != nullptr && curElement->next->value < value)
+    curElement = curElement->next;
+  
+  SetElement *removeElement = curElement->next;
+  curElement->next = curElement->next->next;
+  delete removeElement;
+}
+
+template<typename T>
+Set<T> Set<T>::intersec(Set<T> &set)
+{
+  Set<T> newSet;
+  SetElement *firstElement = head->next;
+  SetElement *secondElement = set.head->next;
+
+  while (firstElement != nullptr && secondElement != nullptr)
+  {
+    if (firstElement->value == secondElement->value)
+    {
+      newSet.add(firstElement->value);
+      firstElement = firstElement->next;
+      secondElement = secondElement->next;
+    }
+    else if (firstElement->value > secondElement->value)
+      secondElement = secondElement->next;
+    else
+      firstElement = firstElement->next;
+  
+  } 
+
+  return newSet;
+}
+
+template<typename T>
+Set<T> Set<T>::un(Set<T> &set)
+{
+  Set<T> newSet;
+  SetElement *firstElement = head->next;
+  SetElement *secondElement = set.head->next;
+
+  while (firstElement != nullptr)
+  {
+    newSet.add(firstElement->value);
+    firstElement = firstElement->next;
+  }
+  while (secondElement != nullptr)
+  {
+    newSet.add(secondElement->value);
+    secondElement = secondElement->next;
+  }
+
+  return newSet;
+}
