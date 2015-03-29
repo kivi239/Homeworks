a20eaae1a78c11ee818514334e9c873c5412a835
kivi239 <ilya.kuzmina@yandex.ru>
2015-03-29 16:06:53 +0300

    Task 3.4 is done

--- 4S/03/4/tree.hs
+++ 4S/03/4/tree.hs
@@ -5,3 +5,9 @@ treeHeightHelper :: Tree a -> Int -> Int
 treeHeight t = treeHeightHelper t 0
 treeHeightHelper (Leaf x) h = h
 treeHeightHelper (Branch l x r) h = 1 + max (treeHeightHelper l h) (treeHeightHelper r h) 
+
+minDistToLeaf :: Tree a -> Int
+minDistToLeafHelper :: Tree a -> Int -> Int
+minDistToLeaf t = minDistToLeafHelper t 0
+minDistToLeafHelper (Leaf a) d = d
+minDistToLeafHelper (Branch l x r) d = 1 + min (minDistToLeafHelper l d) (minDistToLeafHelper r d)
