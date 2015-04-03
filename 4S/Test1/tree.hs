d164a28ba435d70c0bd2d534cd2b766a08c6605a
kivi239 <ilya.kuzmina@yandex.ru>
2015-04-03 12:43:50 +0300

    tabs

--- 4S/Test1/tree.hs
+++ 4S/Test1/tree.hs
@@ -6,7 +6,7 @@ findHelper :: Tree Int -> [Int] -> [Int]
 findNegative t = findHelper t []
 
 findHelper (Leaf a) l = if (a < 0) then (a : l)
-						  else l
+						else l
 findHelper (Branch l a r) list = if (a < 0) then findHelper r (a : (findHelper l list))
-							else findHelper r (findHelper l list)
+								 else findHelper r (findHelper l list)
 
