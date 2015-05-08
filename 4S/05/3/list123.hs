e02e9d78ec0d63aa088bf06fa6c1b3ba7e5cea22
kivi239 <ilya.kuzmina@yandex.ru>
2015-05-09 00:02:53 +0300

    Task 5.3 fixed

--- 4S/05/3/list123.hs
+++ 4S/05/3/list123.hs
@@ -1,4 +1,5 @@
 list123 :: Int -> [[Int]]
-
-list123 0 = [[]]
-list123 n = concatMap (\x -> [x ++ [1], x ++ [2], x ++ [3]]) (list123 (n - 1))
+list123' :: Int -> [[Int]]
+list123 n = map reverse (list123' n)
+list123' 0 = [[]]
+list123' n = concatMap (\x -> [1:x, 2:x, 3:x]) (list123' (n - 1))
